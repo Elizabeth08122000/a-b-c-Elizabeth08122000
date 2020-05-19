f42ee66a86660a70e41de9676f08a7f90a2b5934
Liza <bliza0812@gmail.com>
2020-05-19 14:33:22 +0400

    Answer

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        float C = in.nextFloat();
+        System.out.println((A+B)*C);
+        in.close();
     }
 }
